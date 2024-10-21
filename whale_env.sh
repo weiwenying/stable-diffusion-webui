7a14555db523d764f6d7d87a4350984b84f6a98c
wenying wei <weiwenying520@gmail.com>
2024-10-21 19:20:18 +0800

    update whale_env.sh

--- whale_env.sh
+++ whale_env.sh
@@ -3,3 +3,7 @@ echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
 pip install git+http://gitlab.iiva.org.cn/nuvic/2024/aigcapi.git@mysql
 
 git pull origin nuvic
+
+git fetch --tags
+
+git checkout -b runtime tags/${1:-"whale-sdwebui-v0.0.3"}
