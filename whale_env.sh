175ce759152c54689f8320ca920d8006fa524149
wenying wei <weiwenying520@gmail.com>
2024-10-24 13:58:41 +0800

    udpate

--- whale_env.sh
+++ whale_env.sh
@@ -1,8 +1,5 @@
 
 pip install git+http://gitlab.iiva.org.cn/nuvic/2024/aigcapi.git@mysql
-
-git checkout nuvic && git pull iiva nuvic
-
+# git checkout nuvic && git pull iiva nuvic
 git fetch --tags
-
 git checkout -b runtime ${1:-"nuvic"}
--- whale_env_update.sh
+++ whale_env_update.sh
@@ -1,5 +1,6 @@
 echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
 
-wget http://gitlab.iiva.org.cn/nuvic/2024/stable-diffusion-webui/-/raw/nuvic/whale_env.sh?inline=false -O whale_env.sh
+# wget http://gitlab.iiva.org.cn/nuvic/2024/stable-diffusion-webui/-/raw/nuvic/whale_env.sh?inline=false -O whale_env.sh
+git checkout nuvic && git pull iiva nuvic
 
 chmod +x whale_env.sh
