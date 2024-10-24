3fcaec2bfb97e5b16f4f31ac977b85fe34b38d1b
wenying wei <weiwenying520@gmail.com>
2024-10-24 18:21:53 +0800

    update

--- whale_env.sh
+++ whale_env.sh
@@ -1,5 +1,18 @@
+SDWEBUI_VERSION=${1:-"nuvic"}
 
-pip install git+http://gitlab.iiva.org.cn/nuvic/2024/aigcapi.git@mysql
-# git checkout nuvic && git pull iiva nuvic
-git fetch --tags
-git checkout -b runtime ${1:-"nuvic"}
+echo $SDWEBUI_VERSION
+
+case "$SDWEBUI_VERSION" in
+    "whale-sdwebui-v0.0.2" | "whale-sdwebui-v0.0.3" | "whale-sdwebui-v0.0.4")
+        echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
+        pip install git+http://gitlab.iiva.org.cn/nuvic/2024/aigcapi.git@mysql
+        git pull origin nuvic
+        git fetch --tags
+        git checkout -b runtime ${1:-"nuvic"}
+        ;;
+    *)
+        pip install git+http://gitlab.iiva.org.cn/nuvic/2024/aigcapi.git@mysql
+        git fetch --tags
+        git checkout -b runtime ${1:-"nuvic"}
+        ;;
+esac
