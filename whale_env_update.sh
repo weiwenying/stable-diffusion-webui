aa7b008cdbfced89f789fab6fcdb7c0a2bbd2b52
wenying wei <weiwenying520@gmail.com>
2024-10-25 13:46:33 +0800

    update

--- whale_env_update.sh
+++ whale_env_update.sh
@@ -3,4 +3,5 @@ echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
 # wget http://gitlab.iiva.org.cn/nuvic/2024/stable-diffusion-webui/-/raw/nuvic/whale_env.sh?inline=false -O whale_env.sh
 git checkout nuvic && git pull iiva nuvic
 
-chmod +x ./whale_env.sh && ./whale_env.sh $@
+# chmod +x ./whale_env.sh && ./whale_env.sh $@
+chmod +x ./whale_env.sh 
