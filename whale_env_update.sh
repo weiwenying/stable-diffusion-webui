f46c83a32623330f4ac595da914fbd7ae2da1605
wenying wei <weiwenying520@gmail.com>
2024-10-25 17:14:04 +0800

    update

--- whale_env_update.sh
+++ whale_env_update.sh
@@ -1,5 +1,5 @@
 
-echo "启动whale_env_pudate.sh"
+printf "\n\n更新脚本(whale_env_pudate.sh):\n"
 echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
 echo "git checkout nuvic && git pull origin nuvic"
 # wget http://gitlab.iiva.org.cn/nuvic/2024/stable-diffusion-webui/-/raw/nuvic/whale_env.sh?inline=false -O whale_env.sh
@@ -7,4 +7,4 @@ git checkout nuvic && git pull origin nuvic
 
 # chmod +x ./whale_env.sh && ./whale_env.sh $@
 chmod +x ./whale_env.sh 
-echo "whale_env_pudate.sh完毕"
+printf "脚本更新完成\n\n"
