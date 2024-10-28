678aef4b4e70e148cc691e4b52e79535d16ee170
wenying wei <weiwenying520@gmail.com>
2024-10-28 17:11:26 +0800

    unintall aigcapi before install aigcapi.

--- whale_env.sh
+++ whale_env.sh
@@ -2,6 +2,8 @@ SDWEBUI_VERSION=${1:-"nuvic"}
 
 echo $SDWEBUI_VERSION
 
+pip uninstall aigcapi
+
 case "$SDWEBUI_VERSION" in
     "whale-sdwebui-v0.0.2" | "whale-sdwebui-v0.0.3" | "whale-sdwebui-v0.0.4")
         echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
