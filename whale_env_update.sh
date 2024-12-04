701754ab2f9cb42c2af8666eeff9d72b20aa888f
wenying wei <weiwenying520@gmail.com>
2024-12-04 17:45:29 +0800

    merge remote force. remote branch is be using.

--- whale_env_update.sh
+++ whale_env_update.sh
@@ -3,7 +3,7 @@ printf "\n\n更新脚本(whale_env_pudate.sh):\n"
 echo "10.1.252.5  gitlab.iiva.org.cn"  >> /etc/hosts
 echo "git checkout nuvic && git pull origin nuvic"
 # wget http://gitlab.iiva.org.cn/nuvic/2024/stable-diffusion-webui/-/raw/nuvic/whale_env.sh?inline=false -O whale_env.sh
-git checkout nuvic && git pull origin nuvic
+git checkout nuvic && git fetch origin && git reset --hard origin/nuvic
 
 # chmod +x ./whale_env.sh && ./whale_env.sh $@
 chmod +x ./whale_env.sh 
